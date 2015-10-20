65ad436c6129c059de31407b58b31fca65de926c
Nick Pillitteri <nickp@smartertravelmedia.com>
2015-10-20 18:20:00 -0400

    Move conditional to push-docs.sh

--- push-docs.sh
+++ push-docs.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
-mvn -P publish -D github.site.oauth2Token=$GH_CI_TOKEN clean site-deploy
-
+if [[ "$TRAVIS_REPO_SLUG" == "smarter-travel-media/st-metrics" ]] && [[ "$TRAVIS_BRANCH" == "master" ]] && [[ "$TRAVIS_PULL_REQUEST" == "false" ]]; then
+    mvn -P publish -D github.site.oauth2Token=$GH_CI_TOKEN clean site-deploy
+fi
