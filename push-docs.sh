b669ab0452d2cfd76aff2c5af26600f6269da0c7
Nick Pillitteri <nickp@smartertravelmedia.com>
2017-02-28 11:38:07 -0500

    Remove auto generation of docs

--- push-docs.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-if ([ "$TRAVIS_REPO_SLUG" = "smarter-travel-media/st-metrics" ] && [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ]); then
-    mvn -P publish -D github.site.oauth2Token=$GH_CI_TOKEN clean site-deploy
-fi
